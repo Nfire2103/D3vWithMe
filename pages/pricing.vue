fb960dfe52b9eb5748a66e7a27ec79df9ae4d80f
Vianney <vianney.touillon@epitech.eu>
2024-03-24 01:51:20 +0100

    [ADD] modified navbar

--- pages/pricing.vue
+++ pages/pricing.vue
@@ -28,34 +28,6 @@ const isYearly = ref(false);
 
 <template>
   <div v-if="page">
-    <UPageHero v-bind="page.hero">
-      <template #links>
-        <UPricingToggle v-model="isYearly" class="w-48" />
-      </template>
-    </UPageHero>
-
-    <UContainer>
-      <UPricingGrid>
-        <UPricingCard
-          v-for="(plan, index) in page.plans"
-          :key="index"
-          v-bind="plan"
-          :price="isYearly ? plan.price.year : plan.price.month"
-          :cycle="isYearly ? '/year' : '/month'"
-        />
-      </UPricingGrid>
-    </UContainer>
-
-    <ULandingSection>
-      <ULandingLogos>
-        <UIcon
-          v-for="icon in page.logos.icons"
-          :key="icon"
-          :name="icon"
-          class="w-12 h-12 flex-shrink-0 text-gray-500 dark:text-gray-400"
-        />
-      </ULandingLogos>
-    </ULandingSection>
 
     <ULandingSection
       :title="page.faq.title"
